80dcd1ce2b55e8dd2d425572fc47dbba3b6a256d
unknown <dk234@.(none)>
2010-10-31 11:41:30 -0400

    Added some more explanatory comments.

--- src/main/clojure/com/quephird/pe/solutions/206.clj
+++ src/main/clojure/com/quephird/pe/solutions/206.clj
@@ -2,6 +2,12 @@
   (:use com.quephird.pe.util)
 )
 
+; We only consider the range of numbers whose minimum square is just below
+; 1020304050607080900 and whose maximum is just over 192939495969798990.
+; This is only a small optimization over simply computing the set of all
+; possible combinations of numbers with this same pattern, but it is much
+; easier to produce a comparable set of test cases.
+;
 ; Note that our range only includes multiples of ten since its square must end in 0.
 (defn answer []
   (first-value-of
